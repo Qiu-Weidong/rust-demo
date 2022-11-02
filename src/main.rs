6c7891a85bdb3d0908bb8e0e740316c1714f88bc
unknown <568385476@qq.com>
2022-11-02 15:32:09 +0800

    .

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use colored::Colorize;
+
+fn main() {
+    println!("　１２３４５６７８９　");
+    println!("　＿＿＿＿＿＿＿＿＿　");
+    println!("｜{}｜", "車馬象士將士象馬車".bright_white());
+    println!("｜　　　　　　　　　｜");
+    println!("｜{}｜", "　砲　　　　　砲　".bright_white());
+    println!("｜{}｜", "卒　卒　卒　卒　卒".bright_white());
+    println!("｜－－－－－－－－－｜");
+    println!("｜－－－－－－－－－｜");
+    println!("｜{}｜", "兵　兵　兵　兵　兵".bright_red());
+    println!("｜{}｜", "　炮　　　　　炮　".bright_red());
+    println!("｜　　　　　　　　　｜");
+    println!("｜{}｜", "俥傌相仕帥仕相傌俥".bright_red());
+    println!("　￣￣￣￣￣￣￣￣￣　");
+    println!("　１２３４５６７８９　");
+    // let black = "black".black();
+    // let blue = "blue".blue();
+    // let bright_blue = "bright".bright_blue();
+
+    // println!("{}, {}, {}", black, blue, bright_blue);
+}
+/*
+a̲b̲
+unicode符号
+１２３４５６７８９０
+　｜＋＿－￣
+
+方案一　　　　　　　　　　　　　方案二　　　　　　　　　　　　　　
+　１２３４５６７８９　　　　　　　１２３４５６７８９ 　　　　　　　　　　　　　　　　　　　　
+　＿＿＿＿＿＿＿＿＿　　　　　　　＿＿＿＿＿＿＿＿＿　　　　　　　　
+｜車馬象士將士象馬車｜　　　　　｜車馬象士將士象馬車｜　　　　　　　　
+｜　　　　　　　　　｜　　　　　｜＋＋＋＋＋＋＋＋＋｜　　　　　　　　
+｜　砲　　　　　砲　｜　　　　　｜＋砲＋＋＋＋＋砲＋｜　　　　　　　　
+｜卒　卒　卒　卒　卒｜　　　　　｜卒＋卒＋卒＋卒＋卒｜　　　　　　　　
+｜－－－－－－－－－｜　　　　　｜－－－－－－－－－｜　　　　　　　　
+｜－－－－－－－－－｜　　　　　｜－－－－－－－－－｜　　　　　　　　
+｜兵　兵　兵　兵　兵｜　　　　　｜兵＋兵＋兵＋兵＋兵｜　　　　　　　　
+｜　炮　　　　　炮　｜　　　　　｜＋炮＋＋＋＋＋炮＋｜　　　　　　　　
+｜　　　　　　　　　｜　　　　　｜＋＋＋＋＋＋＋＋＋｜　　　　　　　　
+｜俥傌相仕帥仕相傌俥｜　　　　　｜俥傌相仕帥仕相傌俥｜　　　　　　　　
+　￣￣￣￣￣￣￣￣￣　　　　　　　￣￣￣￣￣￣￣￣￣　　　　　　　
+　１２３４５６７８９　　　　　　　１２３４５６７８９ 　
+
+
+
+*/
